c8c70be34abbbc844e1d835aa6f97627ef19e551
pzhuchkov <pzhuchkov@gmail.com>
2021-01-24 23:21:48 +0300

    Rename variables related with decoding response to json

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ def get_wall_upload_server(group_id=VK_GROUP_ID, url=VK_API_URL):
     response = requests.get(url.format('photos.getWallUploadServer'),
                             params=params)
     response.raise_for_status()
-    upload_url = response.json()['response']['upload_url']
+    response_decoded = response.json()
+    upload_url = response_decoded['response']['upload_url']
     return upload_url
 
 
@@ -41,10 +42,10 @@ def upload_and_save_image(url, file):
         files = {'photo': file}
         response = requests.post(url, files=files)
         response.raise_for_status()
-        uploading_properties = response.json()
-    owner_id, media_id = save_image(uploading_properties['server'],
-                                    uploading_properties['photo'],
-                                    uploading_properties['hash'])
+        response_decoded = response.json()
+    owner_id, media_id = save_image(response_decoded['server'],
+                                    response_decoded['photo'],
+                                    response_decoded['hash'])
     return owner_id, media_id
 
 
@@ -61,8 +62,8 @@ def save_image(server, photo, hash_code,
     response = requests.post(url.format('photos.saveWallPhoto'),
                              params=params)
     response.raise_for_status()
-    saving_properties = response.json()['response'][0]
-    return saving_properties['owner_id'], saving_properties['id']
+    response_decoded = response.json()['response'][0]
+    return response_decoded['owner_id'], response_decoded['id']
 
 
 def make_publication(owner_id, media_id, title, comments,
@@ -91,8 +92,8 @@ def get_comic_properties(comic_id, url=COMIC_API_URL):
     url = url.format(comic_id)
     response = requests.get(url)
     response.raise_for_status()
-    comic_properties = response.json()
-    return comic_properties
+    response_decoded = response.json()
+    return response_decoded
 
 
 def main():
